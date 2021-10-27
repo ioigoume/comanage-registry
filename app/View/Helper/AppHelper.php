96fb9e6675a14366347937b289103f8520a5d6b2
lionick <lionick85@hotmail.com>
2021-10-27 06:57:06 +0000

    Convert petition comment to textarea
    minor fixes

--- app/View/Helper/AppHelper.php
+++ app/View/Helper/AppHelper.php
@@ -2,4 +2,12 @@
 
 App::uses('Helper', 'View');
 class AppHelper extends Helper {
+    function truncateString($string, $limit = 100) {
+        $stringCut = substr($string, 0, $limit);
+        $endPoint = strrpos($stringCut, ' ');
+
+        //if the string doesn't contain any space then it will cut without word basis.
+        $string = $endPoint ? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
+        return $string;
+    }
 }
